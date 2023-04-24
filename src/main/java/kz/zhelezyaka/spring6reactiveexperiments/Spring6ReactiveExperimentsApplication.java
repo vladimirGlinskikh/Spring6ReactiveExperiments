f20648c44e26854941508f0598aa63469f9f86d0
Vladimir Glinskikh <vladimir.v.glinskikh@gmail.com>
2023-04-24 17:34:30 +0300

    Start project

---
+++ src/main/java/kz/zhelezyaka/spring6reactiveexperiments/Spring6ReactiveExperimentsApplication.java
@@ -0,0 +1,13 @@
+package kz.zhelezyaka.spring6reactiveexperiments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring6ReactiveExperimentsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring6ReactiveExperimentsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kz/zhelezyaka/spring6reactiveexperiments/Spring6ReactiveExperimentsApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.zhelezyaka.spring6reactiveexperiments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring6ReactiveExperimentsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
