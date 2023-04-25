15031364636181a3ab0167ef4a77c769e2a19d2b
Vladimir Glinskikh <vladimir.v.glinskikh@gmail.com>
2023-04-25 05:30:36 +0300

    Flux Operations

--- src/test/java/kz/zhelezyaka/spring6reactiveexperiments/repositories/PersonRepositoryImplTest.java
+++ src/test/java/kz/zhelezyaka/spring6reactiveexperiments/repositories/PersonRepositoryImplTest.java
@@ -2,7 +2,10 @@ package kz.zhelezyaka.spring6reactiveexperiments.repositories;
 
 import kz.zhelezyaka.spring6reactiveexperiments.domain.Person;
 import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+
+import java.util.List;
 
 class PersonRepositoryImplTest {
     PersonRepository personRepository = new PersonRepositoryImpl();
@@ -25,9 +28,42 @@ class PersonRepositoryImplTest {
     @Test
     void testMapOperation() {
         Mono<Person> personMono = personRepository.getById(2);
-        personMono.map(person -> person.getFirstName())
+        personMono.map(Person::getFirstName)
                 .subscribe(firstName -> {
                     System.out.println(firstName);
                 });
+    }
+
+    @Test
+    void testFluxBlockFirst() {
+        Flux<Person> personFlux = personRepository.findAll();
+
+        Person person = personFlux.blockFirst();
+
+        System.out.println(person.toString());
+    }
+
+    @Test
+    void testFluxSubscriber() {
+        Flux<Person> personFlux = personRepository.findAll();
+        personFlux.subscribe(person -> {
+            System.out.println(person.toString());
+        });
+    }
+
+    @Test
+    void testFluxMap() {
+        Flux<Person> personFlux = personRepository.findAll();
+        personFlux.map(Person::getFirstName)
+                .subscribe(firstName -> System.out.println(firstName));
+    }
+
+    @Test
+    void testFluxToList() {
+        Flux<Person> personFlux = personRepository.findAll();
+        Mono<List<Person>> listMono = personFlux.collectList();
+        listMono.subscribe(list -> {
+            list.forEach(person -> System.out.println(person.getFirstName()));
+        });
     }
 }
